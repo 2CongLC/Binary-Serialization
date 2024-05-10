ad0368c11dbaddccf3bca82fe576df07c5838248
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-05-10 15:00:40 +0700

    Update SerializationManager.vb

--- Binary Serialization/SerializationManager.vb
+++ Binary Serialization/SerializationManager.vb
@@ -26,4 +26,32 @@ Public Class SerializationManager(Of t As Class)
         End Try
     End Sub
 
+
+    <Obsolete>
+    Public Shared Function BinarySerialize(ByVal obj As t) As String
+
+        Dim ms As MemoryStream = New MemoryStream()
+        Dim binaryFormatter As New BinaryFormatter()
+        Try
+            binaryFormatter.Serialize(ms, obj)
+        Catch ex As Exception
+        End Try
+        Return Encoding.UTF8.GetString(ms.ToArray())
+
+    End Function
+
+    <Obsolete>
+    Public Shared Function BinaryDeSerialize(ByVal value As String) As t
+
+        Dim ms As MemoryStream = New MemoryStream(Encoding.UTF8.GetBytes(value))
+        Dim binaryFormatter As New BinaryFormatter()
+        Dim obj As t = Nothing
+        Try
+            obj = DirectCast(binaryFormatter.Deserialize(ms), t)
+        Catch ex As Exception
+        End Try
+        Return obj
+
+    End Function
+        
 End Class
